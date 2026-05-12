f5a740887d043fcff5a471e0a55d73c96cf56023
Kambiz Aghaiepour <kambiz@redhat.com>
2026-05-12 10:28:09 -0400

    fix black formatting
    only run truststore related code on darwin.

--- src/quads_client/connection.py
+++ src/quads_client/connection.py
@@ -3,7 +3,7 @@ import sys
 import jwt
 import urllib3
 Conditionally inject truststore only on macOS
-if sys.platform == 'darwin':
+if sys.platform == "darwin":
     try:
         import truststore
         _has_truststore = True
@@ -146,8 +146,9 @@ class ConnectionManager:
         username, password = self.config.get_server_credentials(server_name)
         verify = self.config.get_server_verify(server_name)
 
-        if sys.platform == 'darwin' and _has_truststore:
-            truststore.inject_into_ssl()
+        if sys.platform == "darwin":
+            if _has_truststore:
+                truststore.inject_into_ssl()
 
         # Suppress SSL warnings when certificate verification is disabled
         if not verify:
